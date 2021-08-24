2a95365c8317fa9ea4121810ebf2db4c8e9e71c0
niqita21 <niqitatewari@gmail.com>
2021-08-24 15:24:46 +0530

    Gitpracticefiles

---
+++ src/test/java/Githubpractice/Sample1.java
@@ -0,0 +1,7 @@
+package Githubpractice;
+
+public class Sample1 {
+public static void main(String[] args) {
+	System.out.println("heyy how is your day?");
+}
+}
---
+++ src/test/java/Githubpractice/Sample3.java
@@ -0,0 +1,7 @@
+package Githubpractice;
+
+public class Sample3 {
+public static void main(String[] args) {
+	System.out.println("how are you?");
+}
+}
